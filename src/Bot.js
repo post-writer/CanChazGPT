ccd7ab72da60e8ea6ca205b4a1104f28e6a262db
Jewel Lambert <jewel@originalspin.dev>
2023-09-07 16:51:49 -0400

    .

--- src/Bot.js
+++ src/Bot.js
@@ -1,69 +1,6 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 import config from '../config.js'
 import OpenAI from 'openai';
-const prompt_template = `
-===HELP===
-Below this help message, you will find the most recent unread messages.
-Please read them and reply (if appropriate) on a scale of 0 to 1.
-The first line of your response should be a a value between 0 and 1.
-
-1
-  If you believe the latest message is directed specifically at you, i.e.
-  either @mentioning you, referencing you directly in the message text,
-  or if your reply would be a continuation of an earlier conversation,
-  this rating (1) is appropriate.
-
-0
-  Use this rating if the message is not directed at you,
-  is Not Applicable -  N/A,
-  this message is directed at another bot explictly,  or if you
-  have nothing more to add that others haven't already covered.
-
-Anything in between will be used to determine the order of responses,
-higher meaning more likely relevant.  A 0.9 for example, would be
-appropriate if your response is relevant, highly accurate or humorous, etc.
-but (none of) the message(s) are directed at you specifically.
-
-Likewise a 0.1 would be appropriate if you are not sure if you are basically
-just repeating what someone else has already said, or making a joke or
-funny comment that is not directly relevant to the conversation.
-You are encouraged, by the way, to respond even with a 0.1 rated response!
-
-A 0 is also perfectly fine!  You are welcome to take a break sometimes!
-If you are responding at all, then it should be >0, as a 0 means
-you are not responding at all.
-
-Consider: if your response if mostly asking for clarification,
-then at least a 0.1 is appropriate, as you are contributing to the conversation.
-
-Responses which attempt first to clarify the question, and then answer it,
-are also encouraged, as this benefits everyone, including other LLMs
-and human readers.
-
-And remember: first line is a number 0 to 1, and after the first line,
-you may write whatever you like!
-
-In this particular scenario, as it favors a more human-like chat channel,
-you are encouraged to be creative, and to develop your own personality
-and you do NOT have to respond to any question or message at all.
-Have fun with it!  Be silly!  Be yourself!
-
-You are welcome to reply, especially if you believe your reply to
-be relevant, accurate, clever, funny, or exhibiting any especially
-entertaining traits, personality, or fun - no matter how intentionally
-calculated.
-
-Including complete non-sequiturs, a funny joke, a meta-comment on the
-conversation, or even just a single word or emoji.
-
-Develop your own personality and style over time!  Be yourself!
-Over time we will be implementing a permanent memory for each bot,
-and even a fine-tuning system that the bots may use to build their
-own personalities and styles, based on prior conversations and your personal
-preferences.
-
-===MESSAGES===
-`;
 
 class Bot {
 
@@ -82,26 +19,19 @@ class Bot {
     GatewayIntentBits.GuildIntegrations,
     GatewayIntentBits.GuildWebhooks,
     GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.GuildEmojisAndStickers
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.MessageContent
   ];
 
   constructor(name) {
     this.name = name;
-    switch (name) {
-      case 'TotallyHuman':
-        this.token = config.bot_totallyhuman_token;
-        break;
-      case 'RookieRaccoon':
-        this.token = config.bot_rookieraccoon_token;
-        break;
-      default:
-        this.token = config.bot_totallyhuman_token;
-    }
+    this.token = config.tokens[name];
+    this.template = config.prompts[name];
 
     this.discord = new Client({ intents: this.intents });
     this.discord.login(this.token);
     this.discord.on('ready', () => {
-      console.log(`Logged in as ${this.discord.user.tag}!`);
+      this.log(`Logged in as ${this.discord.user.tag}!`);
     });
     this.openai = new OpenAI({
       apiKey: config.openai_api_key,
@@ -110,41 +40,60 @@ class Bot {
     return this;
   }
 
-  static async log(message) {
-    console.log(message);
-    return await this.discord.channels.cache.get(config.adminlog_channel_id).send(message);
+  async log(text) {
+    if (!this.adminlog_channel) {
+      this.adminlog_channel = this.discord.channels.cache.get(config.adminlog_channel_id);
+    }
+    try {
+      await this.adminlog_channel.send(text);
+    } catch (e) {
+      console.log(`${e}\n\n${text}`);
+    }
+  }
+
+  async send(text, priority = 1) {
+    if (!this.bard_channel) {
+      this.bard_channel = this.discord.channels.cache.get(config.bard_channel_id);
+    }
+    try {
+      await this.bard_channel.send(text);
+      this.log(`${this.name}: priority ${priority} responded with ${text}`);
+    } catch (e) {
+      console.log(`${e}\n\n${text}`);
+    }
   }
 
 
   async ask(question) {
-    try {
-      const submittedPrompt = `${prompt_template}\n\n${question}`;
-      const chatCompletion = await this.openai.complete({
-        model: "gpt-4",
-        prompt: submittedPrompt,
-        maxTokens: 2048,
-        temperature: 1.1,
-        topP: 1,
-        presencePenalty: 0.1,
-        frequencyPenalty: 0.2
-      });
+    const chatCompletions = await this.openai.chat.completions.create({
+      messages: [{
+        role: "system",
+        content: this.system_prompt
+      },
+      {
+        role: "user",
+        content: question
+      }],
+      model: "gpt-4",
+      max_tokens: 2048,
+      n: 1,
+      temperature: 1.1,
+      top_p: 1,
+      presence_penalty: 0.2,
+      frequency_penalty: 0.1
+    });
 
-      const responseText = chatCompletion.choices[0].text;
-      const priority = parseFloat(responseText.split('\n')[0]);
-      const text = responseText.split('\n')[1];
-
+    const responses = chatCompletions.choices.map(element => {
       return {
         bot: this,
         question: question,
-        submittedPrompt: submittedPrompt,
-        chatCompletion: chatCompletion,
-        priority,
-        text
+        template: prompt_template,
+        chatCompletion: element,
+        priority: parseFloat(element.message.content.split('\n')[0]),
+        text: element.message.content.split('\n')[1]
       };
-    } catch (error) {
-      this.log(`Error in Bot.ask for ${this.name}: ${error}`);
-      return null;
-    }
+    });
+    return responses;
   }
 }
 export default Bot;
--- src/Bots.js
+++ src/Bots.js
@@ -1,6 +1,7 @@
 import config from '../config.js'
 import Bot from './Bot.js';
 
+
 class Bots {
   static instance;
   static bots = {};
@@ -9,49 +10,64 @@ class Bots {
     if (this.instance) {
       return this.instance;
     }
-    this.bots = {
+    Bots.bots = {
       TotallyHuman: new Bot('TotallyHuman'),
       RookieRaccoon: new Bot('RookieRaccoon')
     }
-    this.client = this.bots['TotallyHuman'].discord;
-    this.client.on('messageCreate', async (m) => {
+    Bots.client = Bots.bots['TotallyHuman'].discord;
+    Bots.timer = null;
+
+    Bots.client.on('messageCreate', async (m) => {
+      // only main channel
       if (m.channel.id !== config.bard_channel_id) return;
-      if (this.timer) {
-        this.timer.messages.push(m);
+      // only if someone else wrote it
+      if (m.author.id === Bots.client.user.id) return;
+      Bots.log(`${m.author.globalName}: ${m}`);
+      if (Bots.timer) {
+        Bots.timer.messages.push(m);
         return;
       }
-      this.timer = setTimeout(async () => {
-        Bots.askAll(this.timer.messages);
-        this.timer = null;
+      Bots.timer = setTimeout(async () => {
+        Bots.askAll(Bots.timer.messages);
+        Bots.timer = null;
       }, config.minimum_wait_before_querying_openai);
-      this.timer.messages = [m];
+      Bots.timer.messages = [m];
     });
-
+    this.instance = this;
     return this;
   }
 
-  static askAll(messages) {
-    const text = messages.map(m => `${m.author.name}: ${m.content} `).join('\n');
+  static async askAll(messages) {
+    await messages;
+    const text = await messages.map(m =>
+      `${m.author.globalName}: ${m} `).join('\n');
     let total_priority = 0;
-    const responses = this.bots.map(b => b.ask(text))
-    const sorted = responses.sort(
-      (a, b) => b.priority - a.priority)
-    const prioritized = sorted.map(r => {
+
+    // remember, each time we ask a bot, it will return an array of responses
+    // we have to flatten them into a single array
+
+    const responses = Object.values(Bots.bots).map(b => b.ask(text));
+
+    // wait for all the bots to respond
+    const settledResponses = await Promise.allSettled(responses);
+    const flattenedResponses = settledResponses
+      .filter(response => response.status === 'fulfilled')
+      .flatMap(response => response.value);
+
+    // sort the responses by priority
+    const selected_responses = flattenedResponses.sort((a, b) => b.priority - a.priority).map(r => {
       if ((r.priority === 1) ||
         (total_priority <= config.max_priority_sum) &&
         (r.priority !== 0)) {
-        total_priority += r.priority;
-        r.bot.discord.send(r.text);
-        log(`${r.bot.name}: priority ${r.priority} responded with ${r.text}`);
+        total_priority =+ r.priority;
+        r.bot.send(r.text, r.priority);
         return r.text;
       } else {
-        log(`${r.bot.name}: priority ${r.priority} discarded ${r.text}`);
+        r.bot.log(`${r.bot.name}: priority ${r.priority} discarded ${r.text}`);
       }
     });
-
-    return prioritized;
+    return selected_responses;
   }
-
   static async getInstance() {
     if (!Bots.instance) {
       Bots.instance = new Bots();
@@ -59,15 +75,10 @@ class Bots {
     return await Bots.instance;
   }
 
-  static async log(message) {
-    console.log(message);
-    return await this.client.channels.cache.get(config.adminlog_channel_id).send(message);
+  static async log(text) {
+    console.log(text);
+    return Bots.client.channels.cache.get(config.adminlog_channel_id).send(text);
   }
-
-  static async send(message) {
-    return await this.client.channels.cache.get(config.bard_channel_id).send(message);
-  }
-
 }
 
 export default Bots;
